59f34f66e2fb7a504c7adafee4b61483fae56b3a
Dário <dariojr1233@gmail.com>
2023-01-25 22:26:07 -0300

    Validações e alertas

--- consultaCep.js
+++ consultaCep.js
@@ -1,51 +1,73 @@
-var CEP
-var response
-_this = this
+var CEP = true;
+var response;
+_this = this;
 
-
-getSearch = function(C){
-
-
-}
+getSearch = function (C) {};
 /* 
-validateMask = function(){
-    CEP = document.getElementById("CEP").value
-    console.log($('#CEP').mask('00000-000')); 
+
+Varificar se o padrão é valid ou não.
+
+document.buscaCep.cep.validity.patternMismatch
+
+
+I Usado para acessar o DOM
+II Name do elemento pai
+III Name do elemento dentro da do elemento pai
+IV propriedade pai 
+V propriedade filha
+
+
+Slice - Fatia o Array 
+
+1º Argumento - indice inicial
+2º Argumento - indice Final
+
+
+document.querySelectorAll('HTML ELEMNT')
+
+
 } */
 
-validateCep = function(){
-    const CEPvalid = document.getElementById('CEP')
-    if(CEPvalid.value.length == 8){
-        let CepTemp = document.getElementById("CEP").value
-        let firstPart = CepTemp.slice(0,5)
-        let lastPart = CepTemp.slice(6,10)
-        document.getElementById("CEP").value = `${firstPart}-${lastPart}`
-            if (document.buscaCep.cep.validity.patternMismatch) {
-                document.getElementById("CEP").value = ''
-            }
+validateCep = function () {
+  const CEPvalid = document.getElementById("CEP");
+  if (CEPvalid.value.length == 8) {
+    let firstPart = CEPvalid.value.slice(0, 5);
+    let lastPart = CEPvalid.value.slice(5, 10);
+    CEPvalid.value = `${firstPart}-${lastPart}`;
+    // Validação se o Cep tá no padrão
+    if (document.buscaCep.cep.validity.patternMismatch) {
+      document.getElementById("CEP").value = "";
+      window.alert("Digite um CEP válido");
+      _this.clearInputs();
+    } else {
+      let CepTemp = CEPvalid.value.replace("-", "");
+      _this.getData(CepTemp);
     }
-    
+  }
 
-    console.log(document.buscaCep.cep.validity.patternMismatch,CEPvalid.value);
-    
-}
+  console.log(document.buscaCep.cep.validity.patternMismatch, CEPvalid.value);
+};
 
+clearInputs = function () {
+  document.querySelectorAll("input").forEach((i) => {
+    i.value = "";
+  });
+};
 
-getData = function(CEP) {
-    let CEPvalid = this.validateCep()
-
-    
-
-
-    fetch(`https://viacep.com.br/ws/${CEP}/json/`)
-    .then(response => response.json())
-    .then( data => {
-        console.log(data);
-      document.getElementById("Lagradouro").value = data.logradouro
-      document.getElementById("Complemento").value = data.complemento
-      document.getElementById("Bairro").value = data.bairro
-      document.getElementById("Cidade").value = data.localidade
-    })
-
-
-}
+getData = function (CEP) {
+  fetch(`https://viacep.com.br/ws/${CEP}/json/`)
+    .then((response) => response.json())
+    .then((data) => {
+      debugger
+      console.log(data);
+      if (data.erro == 'true') {
+        _this.clearInputs();
+        window.alert("CEP inválido");
+      } else {
+        document.getElementById("Lagradouro").value = data.logradouro;
+        document.getElementById("Complemento").value = data.complemento == "" ? "Sem Complemento" : data.complemento;
+        document.getElementById("Bairro").value = data.bairro;
+        document.getElementById("Cidade").value = data.localidade;
+      }
+    });
+};
